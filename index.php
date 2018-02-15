1b597f569203b263c774735e8516eede08ed4fc3
vkagarwal <36510082+vkagarwal@users.noreply.github.com>
2018-02-16 00:01:28 +0530

    change in code pipeline
    changes for code pipeline

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations</h1>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
+        <h1>Congratulations Vikas</h1>
+        <p>Your AWS Elastic Beanstalk PHP application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
 
